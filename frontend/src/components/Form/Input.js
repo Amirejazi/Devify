c69e9734dcbbd226853816918c92ff11af7470bb
Amirhosein <ejaziamirhosein@gmail.com>
2024-08-24 20:06:43 +0330

    dev useForm Hook for Inputs

--- frontend/src/components/Form/Input.js
+++ frontend/src/components/Form/Input.js
@@ -1,11 +1,15 @@
-import React, { useReducer, useState } from 'react'
+import React, { useEffect, useReducer, useState } from 'react'
 import './Input.css'
 import validator from '../../validators/validator'
 
 const inputReducer = (state, action) => {
     switch (action.type) {
         case 'CHANGE':
-            return { value: action.value, isValid: validator(action.value, action.validators) }
+            return {
+                ...state,
+                value: action.value,
+                isValid: validator(action.value, action.validators)
+            }
         default:
             return state
     }
@@ -15,6 +19,14 @@ function Input(props) {
 
     const [mainInput, dispatch] = useReducer(inputReducer, { value: '', isValid: false })
     const [statusClass, setStatusClass] = useState(null)
+
+    const { value, isValid } = mainInput
+    const { id } = props
+    
+    useEffect(() => {
+        props.onInputHandler(id, value, isValid)
+
+    }, [value])
 
     const onChangeHandler = (e) => {
         dispatch({
